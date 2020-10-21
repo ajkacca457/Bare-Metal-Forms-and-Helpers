bedabb508f9418bcf4b7467b3b3454dd77b12c83
Avijit Karmaker <ajkacca@gmail.com>
2020-10-22 00:33:54 +0300

    new rails form add

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,18 @@
 class UsersController < ApplicationController
 
+def index
+
+@users=User.all
+
+end
+
+
 def new
 @user=User.new
+end
+
+def edit
+@user=User.find(params[:id])
 end
 
 def create
@@ -16,6 +27,20 @@ render :new
 end
 end
 
+def show
+@user=User.find(params[:id])
+end
+
+
+def update
+@user=User.find(params[:id])
+if @user.update(user_params)
+flash[:notice]="User was successfully updated"
+redirect_to users_path
+else
+render :edit
+end
+end
 
 
 private
